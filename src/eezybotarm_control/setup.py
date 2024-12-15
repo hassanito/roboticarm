ce42ccdfdac5a4a0414b6bdca234c00880892103
Hassanito <hassanlhage@hotmail.com>
2024-12-14 21:56:04 -0500

    Test URDF for eezbotarm

--- src/eezybotarm_control/setup.py
+++ src/eezybotarm_control/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
-import os
 from glob import glob
+import os
 
 package_name = 'eezybotarm_control'
 
@@ -12,20 +12,27 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # Add this line to include launch files
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py'))
+        # Launch files
+        (os.path.join('share', package_name, 'launch'),
+         glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        # URDF files
+        (os.path.join('share', package_name, 'description'),
+         glob(os.path.join('description', '*.urdf'))),
+        # Mesh files
+        (os.path.join('share', package_name, 'meshes', 'ebamk2'),
+         glob(os.path.join('meshes', 'ebamk2', '*.STL'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='your_name',
     maintainer_email='your_email@example.com',
-    description='EEZYbotARM MK2 controller',
+    description='EEZYbotARM MK2 ROS2 Control Package',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'arm_controller = eezybotarm_control.arm_controller:main',
-            'arm_test_patterns = eezybotarm_control.arm_test_patterns:main'
+            'arm_test_patterns = eezybotarm_control.arm_test_patterns:main',
         ],
     },
 )
